6a5d98cb8d9631d766b4e780c6ce737db6e253d4
Tom Wenseleers <Tom.wenseleers@bio.kuleuven.be>
2021-12-14 03:36:05 +0100

    Updated Omicron SGTF fits

--- omicron_growth rate advantage sgtf data.R
+++ omicron_growth rate advantage sgtf data.R
@@ -35,7 +35,7 @@ plotdir = "omicron_sgtf"
 suppressWarnings(dir.create(paste0(".//plots//",plotdir)))
 
 extrapolate = 30
-date.from = as.Date("2021-10-14")
+date.from = as.Date("2021-09-14")
 date.to = today+extrapolate
 dateseq = as.numeric(seq(date.from, date.to, by=1))
 
@@ -45,30 +45,62 @@ xaxis = scale_x_continuous(breaks=firststofmonth,
                            labels=substring(months(firststofmonth),1,1),
                            expand=c(0,0))
 
-# import SGTF data (now proxy for B.1.1.529 / Omicron)
+# import GISAID data to check prop of sgtf samples that are Omicron in function of time (GISAID_sgtf_isomicron)
+# source(".//parse_GISAID.R") # load GISAID data
+GISAID_sgtf_isomicron = readRDS(".//data//GISAID//GISAID_sgtf_isomicron.rds")
+GISAID_sgtf_isomicron$prop_omicron = GISAID_sgtf_isomicron$Omicron/(GISAID_sgtf_isomicron$Omicron+GISAID_sgtf_isomicron$Other)
+selcountries = c("South Africa", "Scotland", "Belgium", "England")
+GISAID_sgtf_isomicron_subs = GISAID_sgtf_isomicron[GISAID_sgtf_isomicron$country %in% selcountries &
+                                                     GISAID_sgtf_isomicron$date>as.Date("2021-07-01"),]
+
+# logistic fit of proportion of sgtf sequences (with Spike_H69del/Spike_V70del) that are Omicron
+fit_sgtf_isomicron = glm(cbind(Omicron,Other) ~ country*DATE_NUM, family=binomial, data=GISAID_sgtf_isomicron_subs)
+# plot(allEffects(fit_sgtf_isomicron))
+
+fit_sgtf_isomicron_preds = as.data.frame(emmeans(fit_sgtf_isomicron, ~ DATE_NUM*country, at=list(DATE_NUM=as.numeric(seq(as.Date("2021-07-01"),
+                                                                                                                         date.to, by=1))), type="response"))
+fit_sgtf_isomicron_preds$date = as.Date(fit_sgtf_isomicron_preds$DATE_NUM, origin="1970-01-01")
+fit_sgtf_isomicron_preds$country = factor(fit_sgtf_isomicron_preds$country, levels=selcountries)
+
+qplot(data=fit_sgtf_isomicron_preds, x=date, y=prob, ymin=asymp.LCL, ymax=asymp.UCL, geom="blank", fill=country, colour=country) +
+  geom_ribbon(aes(colour=NULL), alpha=I(0.2)) +
+  geom_line() +
+  geom_point(data=GISAID_sgtf_isomicron_subs, aes(y=prop_omicron, ymin=NULL, ymax=NULL)) +
+  facet_wrap(~ country) +
+  theme_hc() +
+  ylab("Proportion of SGTF sequences that are Omicron") +
+  ggtitle("Proportion of SGTF sequences\n(with Spike_H69del/Spike_V70del)\nthat are Omicron", "(GISAID data, logistic fit)") +
+  xaxis
+
+# import variant-specific PCR data or SGTF data (now proxy for B.1.1.529 / Omicron)
+varpcr_dk = read.csv(".//data//omicron_sgtf//variantpcr_denmark.csv") # variant-specfc PCR data for DK
+# sgtf_dk = read.csv(".//data//omicron_sgtf//sgtf_denmark.csv") 
 sgtf_sa = read.csv(".//data//omicron_sgtf//sgtf_south africa.csv") # data traced from graph (data not open at the moment)
 sgtf_sa$date = as.Date(sgtf_sa$date)
-sgtf_sa = sgtf_sa[sgtf_sa$date>=as.Date("2021-11-01"),] # data limited to >= Nov 1 when from sequencing data nearly all S dropout was Omicron
+sgtf_sa = sgtf_sa[sgtf_sa$date>=as.Date("2021-11-05"),] # data limited to >= Nov 1 when from sequencing data nearly all S dropout was Omicron
 # sgtf_eng = read.csv(".//data//omicron_sgtf//sgtf_england.csv") # data traced from Twitter graph
 sgtf_eng = read.csv(".//data//omicron_sgtf//sgtf_england_official.csv") # official data (but closed at the moment, so not on github for now)
-sgtf_eng = sgtf_eng[sgtf_eng$date>=as.Date("2021-12-01"),] # data limited to >= Dec 1 when from sequencing data nearly all S dropout was Omicron
+# sgtf_eng = sgtf_eng[sgtf_eng$date>=as.Date("2021-12-01"),] # data limited to >= Dec 1 when from sequencing data nearly all S dropout was Omicron
 sgtf_scot = read.csv(".//data//omicron_sgtf//sgtf_scotland.csv") 
-# sgtf_dk = read.csv(".//data//omicron_sgtf//sgtf_denmark.csv") 
-varpcr_dk = read.csv(".//data//omicron_sgtf//variantpcr_denmark.csv") # variant-specfc PCR data
 sgtf_be = read.csv(".//data//omicron_sgtf//sgtf_belgium.csv") 
 
 sgtf = rbind(sgtf_sa, sgtf_eng, sgtf_scot, sgtf_be)
-sgtf$omicron = sgtf$sgtf # TO DO: correct count sgtf by prop that can be inferred to be Omicron from GISAID sequencing data (would give slightly higher slope/growth rate advantage)
+sgtf$DATE_NUM = as.numeric(sgtf$date)
+# Omicron count is sgtf count * prop sgtf samples inferred to be Omicron (from logistic fit on GISAID data)
+prop_sgtf_omicron = predict(fit_sgtf_isomicron, newdata=sgtf, type="response")
+sgtf$omicron = sgtf$sgtf * prop_sgtf_omicron
 sgtf$prop_omicron = sgtf$omicron / sgtf$pos_tests
 sgtf$sgtf = NULL
 sgtf$prop_sgtf = NULL
+sgtf$DATE_NUM = NULL
 sgtf = rbind(sgtf, varpcr_dk)
 sgtf$date = as.Date(sgtf$date)
 sgtf$date_num = as.numeric(sgtf$date)
 levels_country = c("South Africa", "England", "Scotland", "Denmark", "Belgium")
 sgtf$country = factor(sgtf$country, levels=levels_country)
-sgtf$prop = sgtf$omicron/sgtf$pos_tests
+sgtf$prop <- sgtf$prop_omicron <- sgtf$omicron/sgtf$pos_tests
 sgtf$non_omicron = sgtf$pos_tests-sgtf$omicron
+sgtf = sgtf[sgtf$date >= as.Date("2021-11-01"),]
 sgtf$obs = as.factor(1:nrow(sgtf)) # for observation-level random effect to take into account overdispersion in binomial GLMM
 sgtf$random = factor(1) # fake random effect to be able to run glmmPQL
 names(sgtf)
@@ -83,8 +115,8 @@ fit_sgtf0 = glm(cbind(omicron, non_omicron) ~ date_num + country, family=binomia
 fit_sgtf1 = glm(cbind(omicron, non_omicron) ~ date_num * country, family=binomial(logit), data=sgtf) 
 AIC(fit_sgtf0, fit_sgtf1)
 #            df      AIC
-# fit_sgtf0  6 820.2803
-# fit_sgtf1 10 623.3438 # fits best
+# fit_sgtf0  6 885.3442
+# fit_sgtf1 10 797.6278 # fits best
 
 summary(fit_sgtf1)
 
@@ -102,18 +134,18 @@ deltar_sgtf_bycountry$country = NULL
 colnames(deltar_sgtf_bycountry) = c("delta_r","delta_r_asymp.LCL","delta_r_asymp.UCL")
 deltar_sgtf_bycountry
 #                delta_r delta_r_asymp.LCL delta_r_asymp.UCL
-# South Africa 0.2884629         0.2185518         0.3583740
-# England      0.4782437         0.4560162         0.5004712
-# Scotland     0.3197687         0.2929992         0.3465382
+# South Africa 0.3429570         0.2555922         0.4303217
+# England      0.3932711         0.3801003         0.4064418
+# Scotland     0.3257174         0.2983505         0.3530843
 # Denmark      0.3299872         0.3153420         0.3446324
-# Belgium      0.2328476         0.2033660         0.2623292
+# Belgium      0.2439724         0.2130805         0.2748644
 
 # mean growth rate advantage of Omicron over Delta across countries (mean difference in growth rate per day)
 deltar_sgtf = as.data.frame(emtrends(fit_sgtf1, ~ date_num, var="date_num", at=list(date_num=max(dateseq))))[,c(2,5,6)] # growth rate advantage of Omicron over Delta
 colnames(deltar_sgtf) = c("delta_r","delta_r_asymp.LCL","delta_r_asymp.UCL")
 deltar_sgtf
 #     delta_r delta_r_asymp.LCL delta_r_asymp.UCL
-# 1 0.329862         0.3129128         0.3468112
+# 1 0.327181         0.3074591         0.3469029
 deltar_sgtf_char = sapply(deltar_sgtf, function (x) sprintf(as.character(round(x,2)), 2) )
 deltar_sgtf_char = paste0(deltar_sgtf_char[1], " [", deltar_sgtf_char[2], "-", deltar_sgtf_char[3],"] 95% CLs")
 
@@ -126,11 +158,11 @@ transmadv_sgtf_by_country = exp(deltar_sgtf_bycountry*4.7)
 colnames(transmadv_sgtf_by_country) = c("transmadv","transmadv_asymp.LCL","transmadv_asymp.UCL")
 transmadv_sgtf_by_country
 #              transmadv transmadv_asymp.LCL transmadv_asymp.UCL
-# South Africa  3.879770            2.793216            5.388990
-# England       9.466369            8.527327           10.508819
-# Scotland      4.494763            3.963376            5.097395
+# South Africa  5.012314            3.324381            7.557284
+# England       6.349487            5.968357            6.754957
+# Scotland      4.622204            4.064323            5.256661
 # Denmark       4.715901            4.402214            5.051940
-# Belgium       2.987341            2.600803            3.431327
+# Belgium       3.147695            2.722303            3.639559
 
 # mean transmission advantage of Omicron over Delta across countries (using generation time of 4.7 days)
 # i.e. mean fold difference in the effective reproduction number Re of Omicron compared to that of Delta
@@ -138,7 +170,7 @@ transmadv_sgtf = exp(deltar_sgtf*4.7)
 colnames(transmadv_sgtf) = c("transmadv","transmadv_asymp.LCL","transmadv_asymp.UCL")
 transmadv_sgtf
 #   transmadv transmadv_asymp.LCL transmadv_asymp.UCL
-# 1  4.713127            4.352239             5.10394
+# 1  4.65411            4.242098            5.106139
 transmadv_sgtf_char = sapply(transmadv_sgtf, function (x) sprintf(as.character(round(x,1)), 1) )
 transmadv_sgtf_char = paste0(transmadv_sgtf_char[1], " [", transmadv_sgtf_char[2], "-", transmadv_sgtf_char[3],"] 95% CLs")
 
@@ -154,9 +186,10 @@ qplot(data=sgtf, x=date, y=prop, geom="point", colour=country, fill=country, siz
   scale_y_continuous( trans="logit", breaks=c(10^seq(-5,0),0.5,0.9,0.99,0.999),
                       labels = c("0.001","0.01","0.1","1","10","100","50","90","99","99.9")) +
   xlim(date.from, date.to) +
-  coord_cartesian(ylim=c(0.001,0.99)) +
+  coord_cartesian(xlim=c(as.Date("2021-10-14"),NA), ylim=c(0.001,0.99)) +
   # scale_size_continuous(range=c()) +
-  ggtitle("Spread of Omicron variant inferred from\nS gene target failure (SGTF) data (SA, UK, BE)\n& variant PCR data (DK)") +
+  ggtitle("Spread of Omicron variant inferred from\nS gene target failure (SGTF) data (SA, UK, BE)\n& variant PCR data (DK)",
+          "(SGTF counts adjusted by proportion that was estimated to be Omicron\nbased on GISAID data, data Lesley Scott & NHLS team, UKHSA,\nPublic Health Scotland, Statens Serum Institut, Federal Test Platform Belgium)") +
   ylab("Share of Omicron among confirmed cases (%)") +
   annotate("text", x = c(as.Date("2021-10-16")), y = c(0.94),
            label = paste0("Avg. growth rate advantage of Omicron over Delta:\n", deltar_sgtf_char,
@@ -164,7 +197,8 @@ qplot(data=sgtf, x=date, y=prop, geom="point", colour=country, fill=country, siz
            color="black", hjust=0, size=3) +
   theme_hc() +
   theme(legend.position="right") +
-  xlab("") + xaxis
+  xlab("") + xaxis +
+  theme(plot.subtitle=element_text(size=8))
 
 ggsave(file=paste0(".\\plots\\",plotdir,"\\spread omicron logistic fit sgtf data.png"), width=8, height=6)
 
@@ -175,17 +209,19 @@ qplot(data=sgtf, x=date, y=100*prop, geom="point", colour=country, fill=country,
   # scale_y_continuous( trans="logit", breaks=c(10^seq(-5,0),0.5,0.9,0.99,0.999),
   #                    labels = c("0.001","0.01","0.1","1","10","100","50","90","99","99.9")) +
   xlim(date.from, date.to) +
-  coord_cartesian(ylim=c(0,100)) +
+  coord_cartesian(xlim=c(as.Date("2021-10-14"),NA), ylim=c(0,100)) +
   # scale_size_continuous(range=c()) +
-  ggtitle("Spread of Omicron variant inferred from SGTF data") +
+  ggtitle("Spread of Omicron variant inferred from\nS gene target failure (SGTF) data (SA, UK, BE)\n& variant PCR data (DK)",
+          "(SGTF counts adjusted by proportion that was estimated to be Omicron\nbased on GISAID data, data Lesley Scott & NHLS team, UKHSA,\nPublic Health Scotland, Statens Serum Institut, Federal Test Platform Belgium)") +
   ylab("Share of Omicron among confirmed cases (%)") +
-  annotate("text", x = c(as.Date("2021-10-16")), y = c(88),
+  annotate("text", x = c(as.Date("2021-10-16")), y = c(80),
            label = paste0("Avg. growth rate advantage of Omicron over Delta:\n", deltar_sgtf_char,
                           " per day\n\nAvg. transmission advantage Omicron over Delta\n(with generation time of 4.7 days):\n", transmadv_sgtf_char),
            color="black", hjust=0, size=3) +
   theme_hc() +
   theme(legend.position="right") +
-  xlab("") + xaxis
+  xlab("") + xaxis +
+  theme(plot.subtitle=element_text(size=8))
 
 ggsave(file=paste0(".\\plots\\",plotdir,"\\spread omicron logistic fit sgtf data_linear scale.png"), width=8, height=6)
 
---
+++ parse_GISAID.R
@@ -0,0 +1,129 @@
+library(readr)
+
+# import GISAID metadata (file version metadata_tsv_2021_12_11.tar.xz)
+message("Parsing GISAID metadata...")
+GISAID = read_tsv(".//data//GISAID//metadata.tsv", col_types = cols(.default = "c"))
+GISAID = as.data.frame(GISAID)
+# only keep human samples
+GISAID = GISAID[GISAID$Host=="Human",] 
+GISAID = GISAID[!is.na(GISAID$Type),] 
+nrow(GISAID) # data frame with 5967874  rows
+names(GISAID)
+head(GISAID$Location)
+head(GISAID$"Collection date")
+head(GISAID$"Pango lineage")
+unique(GISAID$"Virus name")
+unique(GISAID$"Type")
+unique(GISAID$"Pango lineage")
+unique(GISAID$"Pangolin version")
+unique(GISAID$"Variant")
+unique(GISAID$"AA Substitutions")
+head(GISAID$covsurver_prot_mutations)
+head(GISAID$covsurver_uniquemutlist)
+unique(GISAID$covv_sampling_strategy)
+
+# parse dates & remove records with invalid dates
+library(stringr)
+date_isvalid = sapply(GISAID$"Collection date", function (s) str_count(s, pattern = "-")==2)
+GISAID = GISAID[date_isvalid,]
+GISAID$date = as.Date(GISAID$"Collection date") 
+GISAID = GISAID[!is.na(GISAID$date),]
+nrow(GISAID) # 5803521
+
+# only keep complete genomes
+GISAID = GISAID[GISAID$"Is complete?"=="True",]
+nrow(GISAID) # 5721143
+
+# add numeric version of date, week midpoint & week & year
+range(GISAID$date) # "2019-12-24" "2021-12-09"
+# GISAID[GISAID$date==min(GISAID$date),] # first available Wuhan genome
+GISAID$Week = lubridate::week(GISAID$date)
+GISAID$Year = lubridate::year(GISAID$date)
+GISAID$Year_Week = interaction(GISAID$Year,GISAID$Week)
+library(lubridate)
+GISAID$week_midpoint = as.Date(as.character(cut(GISAID$date, "week")))+3.5 # week midpoint date
+GISAID$DATE_NUM = as.numeric(GISAID$date)
+names(GISAID)
+
+# parse continent / country / location (PS: location is sometimes city & sometimes province)
+loc = do.call(cbind, data.table::tstrsplit(unlist(GISAID$Location), "/", TRUE)) # parse location info
+loc = trimws(loc, whitespace = " ")
+unique(loc[,1]) # continent
+loc[,2][loc[,3]=="Scotland"] = "Scotland"
+loc[,2][loc[,3]=="England"] = "England"
+loc[,2][loc[,3]=="Wales"] = "Wales"
+loc[,2][loc[,3]=="Northern Ireland"] = "Northern Ireland"
+loc[,2][loc[,2]=="United Kingdom"] = "England"
+sort(unique(loc[,2])) # country
+
+sort(unique(loc[,3])) # city or province
+
+levels_continents = c("Asia","North America","Europe","Africa","South America","Oceania")
+GISAID$continent = factor(loc[,1], levels=levels_continents)
+GISAID$country = factor(loc[,2])
+levels_countries = levels(GISAID$country)
+GISAID$location = factor(loc[,3])
+levels_locations = levels(GISAID$location)
+
+
+# recode pango lineages as desired
+GISAID = GISAID[!(GISAID$"Pango lineage"=="None"|GISAID$"Pango lineage"==""|is.na(GISAID$"Pango lineage")),]
+nrow(GISAID) # 5672058
+unique(GISAID$"Pango lineage")
+length(unique(GISAID$"Pango lineage")) # 1529 pango lineages
+
+# code Omicron lineage
+GISAID$Omicron = "Other"
+GISAID$Omicron[GISAID$Variant=="VOC Omicron GR/484A (BA.1) first detected in Botswana/Hong Kong/South Africa"] = "Omicron"
+
+# code Delta lineage
+GISAID$Delta = "Other"
+GISAID$Delta[GISAID$Variant=="VOC Delta GK/478K.V1 (B.1.617.2+AY.x) first detected in India"] = "Delta"
+
+# code sgtf (S gene target failure) based on presence of Spike_H69del / Spike_V70del
+GISAID$sgtf = grepl("Spike_H69del",GISAID$"AA Substitutions", fixed=T)|grepl("Spike_V70del",GISAID$"AA Substitutions", fixed=T)
+GISAID$sgtf[GISAID$Omicron=="Omicron"] = TRUE
+GISAID$sgtf[GISAID$Omicron=="Delta"] = TRUE
+GISAID$sgtf[GISAID$sgtf==TRUE] = "sgtf"
+GISAID$sgtf[GISAID$sgtf==FALSE] = "non_sgtf"
+
+GISAID_sgtf_isomicron = as.data.frame(ftable(formula=Omicron~country+date, data=GISAID[GISAID$sgtf=="sgtf",]))
+library(tidyr)
+# proportion of sgtf samples that are omicron in function of time by country & date to run logistic regression on
+GISAID_sgtf_isomicron = spread(GISAID_sgtf_isomicron, Omicron, Freq) 
+GISAID_sgtf_isomicron$date = as.Date(GISAID_sgtf_isomicron$date)
+GISAID_sgtf_isomicron$DATE_NUM = as.numeric(GISAID_sgtf_isomicron$date)
+
+
+# # code main clades & VOCs only # TO DO: FINISH THIS PART
+# unique(GISAID$Clade)
+# unique(GISAID$Variant)
+# GISAID$VOC = GISAID$Variant
+# GISAID$VOC[GISAID$covv_lineage=="A"] = "A" 
+# GISAID$VOC[GISAID$VOC==""&(grepl("S",GISAID$covv_clade)|grepl("A.",GISAID$covv_lineage,fixed=T))] = "A*" # = 19B, cf https://www.news-medical.net/health/Viral-Clades-of-SARS-CoV-2.aspx
+# GISAID$VOC[GISAID$covv_lineage=="B"] = "B"
+# GISAID$VOC[GISAID$VOC==""&(grepl("L|O|V|G",GISAID$covv_clade)|grepl("B.",GISAID$covv_lineage,fixed=T))&(!grepl("D614G",GISAID$covsurver_prot_mutations))] = "B*" # B* without D614G
+# GISAID$VOC[GISAID$VOC==""&(grepl("G",GISAID$covv_clade)|grepl("B.",GISAID$covv_lineage,fixed=T)|grepl("AE.",GISAID$covv_lineage,fixed=T)|grepl("AH.",GISAID$covv_lineage,fixed=T)|grepl("AK.",GISAID$covv_lineage,fixed=T)|grepl("AN.",GISAID$covv_lineage,fixed=T)|grepl("AS.",GISAID$covv_lineage,fixed=T)|grepl("AU.",GISAID$covv_lineage,fixed=T)|grepl("AV.",GISAID$covv_lineage,fixed=T)|grepl("AZ.",GISAID$covv_lineage,fixed=T))&(grepl("D614G",GISAID$covsurver_prot_mutations))] = "B* (+S:D614G)" # B* with D614G
+# GISAID$VOC[grepl("Alpha",GISAID$VOC)] = "Alpha"
+# GISAID$VOC[grepl("Beta",GISAID$VOC)] = "Beta"
+# GISAID$VOC[grepl("Gamma",GISAID$VOC)|grepl("P.1.",GISAID$covv_lineage,fixed=T)] = "Gamma"
+# GISAID$VOC[grepl("Delta",GISAID$VOC)|grepl("AY.",GISAID$covv_lineage,fixed=T)] = "Delta"
+# GISAID$VOC[grepl("Lambda",GISAID$VOC)] = "Lambda"
+# GISAID$VOC[grepl("Mu",GISAID$VOC)] = "Mu"
+# table(GISAID$VOC)
+# table(GISAID$covv_lineage[GISAID$VOC==""])  
+# # sum(GISAID$VOC=="") # 0 remaining unassigned
+# # GISAID = GISAID[GISAID$VOC!="",] # we remove these samples; instead we could also recode them as category "other"
+# nrow(GISAID) # 3981577
+# 
+# table(GISAID$covv_lineage[GISAID$covv_location=="Asia / China / Hubei / Wuhan"])
+# 
+# # use main clades/VOCs as my LINEAGE variable
+# levels_LINEAGE = c("A", "A*", "B", "B*", "B* (+S:D614G)", "Alpha", "Beta", "Gamma", "Delta", "Lambda", "Mu")
+# GISAID$LINEAGE = factor(GISAID$VOC, 
+#                              levels=levels_LINEAGE)
+
+
+
+
+
