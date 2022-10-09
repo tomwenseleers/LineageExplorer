6f4ec071bf352ec9971cfb07e00c3781578f59a6
Tom Wenseleers <Tom.wenseleers@bio.kuleuven.be>
2022-10-09 21:29:50 +0200

    Updated GISAID JSON download function

--- download_GISAID_json.R
+++ download_GISAID_json.R
@@ -10,11 +10,13 @@
 library(curl)
 library(glue)
 
+target_dir = "C:/Users/bherr/OneDrive - KU Leuven/Documents/Github/LineageExplorer/LineageExplorer/data/GISAID" # target download directory GISAID data
+
 # note: set GISAID JSON access credentials first using ####
 # Sys.setenv(GISAIDJSON_USERNAME = "XXX")
 # Sys.setenv(GISAIDJSON_PASSWORD = "XXX")
 # Sys.setenv(GISAIDJSON_STREAM = "XXX")
-source(".//set_GISAID_credentials.R")
+source("..//set_GISAID_credentials.R")
 
 custom_curl <- function(user, pass, feed, dest) {
   custom_handle <- curl::new_handle()
@@ -32,9 +34,41 @@ custom_curl <- function(user, pass, feed, dest) {
 }
 
 message("Downloading GISAID JSON...")
+
 custom_curl(Sys.getenv("GISAIDJSON_USERNAME"), 
             Sys.getenv("GISAIDJSON_PASSWORD"), 
             Sys.getenv("GISAIDJSON_STREAM"), 
-            'data/GISAID_json/provision.json.xz')
+            file.path(target_dir, 'provision.json.xz') )
 message("GISAID JSON download complete")
+
+message("Parsing JSON GISAID records...")
+library(dplyr)
+
+library(jsonlite)
+# GISAID_json = jsonlite::stream_in(gzfile(file.path(target_dir, 'provision.json.xz'))) 
+# this runs out of memory on my 64 Gb laptop :-(
+
+# trying instead like this
+con_in = gzfile(file.path(target_dir, 'provision.json.xz'))
+outfile = file.path(target_dir, 'provision.json.bin')
+con_out = file(out <- outfile, open = "wb")
+stream_in(con_in, handler = function(df){
+  # df <- dplyr::filter(df, XXX) # drop any unneeded columns
+  stream_out(df, con_out, pagesize = 100000)
+}, pagesize = 100000)
+close(con_out)
+# stream it back in
+GISAID_JSON = stream_in(file(out))
+nrow(GISAID_JSON)
+
+
+
+
+handler = function(df){
+  df <- dplyr::filter(df, distance > 1000)
+  df <- dplyr::mutate(df, delta = dep_delay - arr_delay)
+  stream_out(df, con_out, pagesize = 1000)
+}
+
+
 
